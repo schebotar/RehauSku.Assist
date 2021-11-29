3eb118d6db672eb8e27f2b2478b0688141782654
Sergey Chebotar <s.chebotar@gmail.com>
2021-11-29 15:50:24 +0300

    Async/await ready solution init commit

--- Assistant/SkuAssist.cs
+++ Assistant/SkuAssist.cs
@@ -1,38 +1,41 @@
-﻿using AngleSharp;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿//using System;
+//using AngleSharp;
+//using AngleSharp.Dom;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Threading.Tasks;
 
-namespace Rehau.Sku.Assist
-{
-    static class SkuAssist
-    {
-        public async static Task<AngleSharp.Dom.IDocument> GetDocumentAsync(string request, HttpClient httpClient)
-        {
-            string uri = "https://shop-rehau.ru/catalogsearch/result/?q=" + request;
+//namespace Rehau.Sku.Assist
+//{
+//    static class SkuAssist
+//    {
+//        public async static Task<string> GetContent(string request, HttpClient httpClient)
+//        {
+//            string uri = "https://shop-rehau.ru/catalogsearch/result/?q=" + request;
+//            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
 
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-            HttpResponseMessage response = await httpClient.GetAsync(uri);
-            response.EnsureSuccessStatusCode();
+//            return await httpClient.GetStringAsync(uri);
+//        }
 
-            IConfiguration config = Configuration.Default;
-            IBrowsingContext context = BrowsingContext.New(config);
+//        public async static Task<AngleSharp.Dom.IDocument> GetDocument(string source)
+//        {
+//            IConfiguration config = Configuration.Default;
+//            IBrowsingContext context = BrowsingContext.New(config);
 
-            string source = await response.Content.ReadAsStringAsync();
-            return await context.OpenAsync(req => req.Content(source));
-        }
+//            return await context.OpenAsync(req => req.Content(source));
+//        }
 
-        public static IProduct GetProductFromDocument(AngleSharp.Dom.IDocument document)
-        {
-            return document
-                .All
-                .Where(e => e.ClassName == "product-item__desc-top")
-                .Select(e => new Product(e.Children[0].TextContent, e.Children[1].TextContent.Trim(new[] { '\n', ' ' })))
-                // .Where(product => !product.Sku.Any(c => char.IsLetter(c)))
-                .FirstOrDefault();
-        }
-    }
-}
+//        public static IProduct GetProductFromDocument(IDocument document)
+//        {
+//            return document
+//                .All
+//                .Where(e => e.ClassName == "product-item__desc-top")
+//                .Select(e => new Product(e.Children[0].TextContent, e.Children[1].TextContent.Trim(new[] { '\n', ' ' })))
+//                // .Where(product => !product.Sku.Any(c => char.IsLetter(c)))
+//                .FirstOrDefault();
+//        }
+//    }
+//}
 
 
--- Functions.cs
+++ Functions.cs
@@ -1,53 +1,18 @@
-﻿using ExcelDna.Integration;
-using System.Runtime.Caching;
+﻿using System;
+using ExcelDna.Integration;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Rehau.Sku.Assist
 {
-    public class Functions : IExcelAddIn
+    public class Functions
     {
-        private static HttpClient _httpClient;
-        private static ObjectCache _resultCache = MemoryCache.Default;
-
-        public void AutoClose()
-        {
-        }
-        public void AutoOpen()
-        {
-            _httpClient = new HttpClient();
-        }
+        private static HttpClient httpClient = new HttpClient();
 
         [ExcelFunction]
-        public static object RAUNAME(string request)
+        public static async Task<string> RAUNAME(string request)
         {
-            string cachedResult = _resultCache[request] as string;
-
-            if (cachedResult != null)
-            {
-                return cachedResult;
-            }
-
-            else
-            {
-                object result = ExcelAsyncUtil.Run("RAUNAME", null,
-                    delegate
-                {
-                    var document = SkuAssist.GetDocumentAsync(request, _httpClient).Result;
-                    var product = SkuAssist.GetProductFromDocument(document);
-                    return product.ToString();
-                });
-
-                if (result.Equals(ExcelError.ExcelErrorNA))
-                {
-                    return "Загрузка...";
-                }
-
-                else
-                {
-                    _resultCache.Add(request, result, System.DateTime.Now.AddMinutes(20));
-                    return result.ToString();
-                }
-            }
+            throw new NotImplementedException();
         }
     }
 }
