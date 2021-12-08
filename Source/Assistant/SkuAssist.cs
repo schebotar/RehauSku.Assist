bc3421e67f7fd64ff0a7a0c078bbba6fbf971905
Sergey Chebotar <s.chebotar@gmail.com>
2021-12-08 17:22:46 +0300

    Edit IsRehauSku regex expression

--- Source/Assistant/SkuAssist.cs
+++ Source/Assistant/SkuAssist.cs
@@ -104,7 +104,7 @@ namespace RehauSku.Assistant
         }
         public static bool IsRehauSku(this string line)
         {
-            return Regex.IsMatch(line, @"[1]\d{6}[1]\d{3}");
+            return Regex.IsMatch(line, @"^[1]\d{6}[1]\d{3}$");
         }
     }
 }
