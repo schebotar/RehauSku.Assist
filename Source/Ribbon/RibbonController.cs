5a641d83d8e8ee9c977d3867f195447cd1164bef
Sergey Chebotar <s.chebotar@gmail.com>
2021-12-09 14:08:26 +0300

    Add settings button and icons to ribbon

--- Source/Ribbon/RibbonController.cs
+++ Source/Ribbon/RibbonController.cs
@@ -10,14 +10,18 @@ namespace RehauSku.Ribbon
     {
         public override string GetCustomUI(string RibbonID)
         {
+
             return @"
       <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
       <ribbon>
         <tabs>
-          <tab id='tab1' label='REHAU'>
-            <group id='group1' label='Прайс-лист'>
-              <button id='button1' label='Экспорт' onAction='OnButtonPressed'/>
-            </group >
+          <tab id='rau' label='REHAU'>
+            <group id='priceList' label='Прайс-лист'>
+                <button id='exportToPrice' label='Экспорт' size='large' imageMso='PivotExportToExcel' onAction='OnButtonPressed'/>
+            </group>
+            <group id='rausettings' label='Настройки'>
+                <button id='set' label='Настройки' size='large' imageMso='CurrentViewSettings' onAction='OnButtonPressed'/>
+            </group>
           </tab>
         </tabs>
       </ribbon>
