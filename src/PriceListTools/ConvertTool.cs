dca27ebcc3a8377bfe6f4f56481d321b615c4c24
Sergey Chebotar <s.chebotar@gmail.com>
2022-01-28 09:16:10 +0300

    Move ConvertTool to another file

---
+++ src/PriceListTools/ConvertTool.cs
@@ -0,0 +1,22 @@
+﻿namespace RehauSku.PriceListTools
+{
+    internal class ConvertTool : PriceListTool
+    {
+        private Source Current;
+
+        public void GetCurrent()
+        {
+            Current = new Source(ExcelApp.ActiveWorkbook);
+        }
+
+        public void FillTarget()
+        {
+            ExcelApp.ScreenUpdating = false;            
+            FillColumn(Current.SkuAmount, TargetFile.amountCell.Column);
+            FilterByAmount();
+            ExcelApp.ScreenUpdating = true;
+
+            Forms.Dialog.SaveWorkbookAs();
+        }
+    }
+}
--- src/PriceListTools/PriceListTool.cs
+++ src/PriceListTools/PriceListTool.cs
@@ -2,31 +2,9 @@
 using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace RehauSku.PriceListTools
 {
-    internal class ConvertTool : PriceListTool
-    {
-        private Source Current;
-
-        public void GetCurrent()
-        {
-            Current = new Source(ExcelApp.ActiveWorkbook);
-        }
-
-        public void FillTarget()
-        {
-            ExcelApp.ScreenUpdating = false;            
-            FillColumn(Current.SkuAmount, TargetFile.amountCell.Column);
-            FilterByAmount();
-            ExcelApp.ScreenUpdating = true;
-
-            Forms.Dialog.SaveWorkbookAs();
-        }
-    }
-
-
     internal abstract class PriceListTool
     {
         protected private Application ExcelApp = (Application)ExcelDnaUtil.Application;
